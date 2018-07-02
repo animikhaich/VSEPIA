a9467c98b7946a15b3b46bfe449ad2f9ed7dbed0
Animikh Aich <animikhaich@gmail.com>
2018-07-03 02:28:50 +0530

    Add Comments to Veg_price_scrape.py

--- Veg_price_scrape.py
+++ Veg_price_scrape.py
@@ -2,13 +2,17 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
+# Function to scrape vegetable prices from the web
 def veg_prices(url):
+    # Get the HTML and Parse it with Beautiful Soup
     raw_html = urlopen(url)
     html = BeautifulSoup(raw_html, 'html5lib')
 
+    # Parse HTML and extract Information
     table = html.find('div', {'class': 'one_half'}).table.tbody.findAll('tr')
     veg_dict = dict()
 
+    # Save all the Vegetable and their prices in the form of a dictionary
     for row in table:
         veg_dict[row.findAll('td')[1].text.strip().lower()] = float(row.findAll('td')[2].text.strip()[4:])
 
@@ -16,6 +20,7 @@ def veg_prices(url):
 
 
 if __name__ == '__main__':
+    # To visualize the Current prices, Print them on console
     url = 'http://www.truetamil.in/price/vegetable-price-bangalore.php'
     dictionary = veg_prices(url)
     print(dictionary)
